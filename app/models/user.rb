26075ecc6900a20b20342e077110178b7fd00183
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-15 10:52:05 +0530

    Fixed order of itemstin Active Record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
   MIN_PASSWORD_LENGTH = 6
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   MAX_EMAIL_LENGTH = 255
-  has_many :comments, dependent: :destroy
 
+  has_many :comments, dependent: :destroy
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+
   has_secure_password
   has_secure_token :authentication_token
 
